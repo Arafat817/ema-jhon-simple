32374e19f3c286476d00a92e308d8d8deba0bb9d
Arafat817 <gr1arafat@gmail.com>
2023-05-22 14:09:32 +0600

    product data loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
+import Shop from './components/shop/Shop'
+
 
 
 function App() {
@@ -11,6 +11,8 @@ function App() {
   return (
     <div className='App'>
       <Header></Header>
+      <Shop></Shop>
+      
 
     </div>
   )
